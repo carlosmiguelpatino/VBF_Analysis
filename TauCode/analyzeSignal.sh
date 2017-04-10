5c097b72011bc3c24ab22b35e7dc38b59bf7717c
carlosmiguelpatino <cm.patino10@uniandes.edu.co>
2017-04-09 20:20:04 -0500

    add signal points

--- TauCode/analyzeSignal.sh
+++ TauCode/analyzeSignal.sh
@@ -1,15 +1,18 @@
 #Script for running analysis code through signal files
+make compile_ROOT_Delphes
 
-LAST=$1 #Last index of samples
 signalPath='/home/aflorez/HN_VBF_signal'
-outputPath='/home/aflorez/HN_Analysis/TauChannel/signal'
+outputPath='/home/aflorez/HN_Analysis/TauChannel'
 
 
-for i in `seq 0 10 ${LAST}`;
+for i in `seq 0 10 90`;
 do
-  ./PhenoAnalyzer ${signalPath}/VBF_m_n3_1p5_tau_decay_vbfcuts_QCD0_seed${i}/Events/run_01/output_delphes.root ${outputPath}/output_signal_seed${i}.root
+  ./PhenoAnalyzer ${signalPath}/VBF_m_n3_1p5_tau_decay_vbfcuts_QCD0_seed${i}/Events/run_01/output_delphes.root ${outputPath}/signal_1p5TeV/output_signal_seed${i}.root
 done
 
-cd ${outputPath}
+hadd -f ${outputPath}/signal_1p5TeV/signal_1p5TeV_700mass_30MET_60pt.root ${outputPath}/signal_1p5TeV/output_signal_seed*.root
+rm ${outputPath}/signal_1p5TeV/output_signal_seed*.root
 
-hadd output_signal.root *.root
+./PhenoAnalyzer ${signalPath}/VBF_m_n3_1p0_tau_decay_vbfcuts/Events/run_01/output_delphes.root ${outputPath}/signal_1p0TeV/signal_1p0TeV_700mass_30MET_60pt.root 
+
+./PhenoAnalyzer ${signalPath}/VBF_m_n3_1p25_tau_decay_vbfcuts/Events/run_01/output_delphes.root ${outputPath}/signal_1p25TeV/signal_1p25TeV_700mass_30MET_60pt.root
