be10a619292d3a637c65ce54ddf34e8a21d6aabd
carlosmiguelpatino <cm.patino10@uniandes.edu.co>
2017-04-16 22:25:19 -0500

    add 2 TeV sample to analysis

--- TauCode/analyzeSignal.sh
+++ TauCode/analyzeSignal.sh
@@ -10,9 +10,11 @@ do
   ./PhenoAnalyzer ${signalPath}/VBF_m_n3_1p5_tau_decay_vbfcuts_QCD0_seed${i}/Events/run_01/output_delphes.root ${outputPath}/signal_1p5TeV/output_signal_seed${i}.root
 done
 
-hadd -f ${outputPath}/signal_1p5TeV/signal_1p5TeV_700mass_30MET_60pt.root ${outputPath}/signal_1p5TeV/output_signal_seed*.root
+hadd -f ${outputPath}/signal_1p5TeV/signal_1p5TeV_700mass_30MET_50pt1.root ${outputPath}/signal_1p5TeV/output_signal_seed*.root
 rm ${outputPath}/signal_1p5TeV/output_signal_seed*.root
 
-./PhenoAnalyzer ${signalPath}/VBF_m_n3_1p0_tau_decay_vbfcuts/Events/run_01/output_delphes.root ${outputPath}/signal_1p0TeV/signal_1p0TeV_700mass_30MET_60pt.root 
+./PhenoAnalyzer ${signalPath}/VBF_m_n3_1p0_tau_decay_vbfcuts/Events/run_01/output_delphes.root ${outputPath}/signal_1p0TeV/signal_1p0TeV_700mass_30MET_50pt1.root 
 
-./PhenoAnalyzer ${signalPath}/VBF_m_n3_1p25_tau_decay_vbfcuts/Events/run_01/output_delphes.root ${outputPath}/signal_1p25TeV/signal_1p25TeV_700mass_30MET_60pt.root
+./PhenoAnalyzer ${signalPath}/VBF_m_n3_1p25_tau_decay_vbfcuts/Events/run_01/output_delphes.root ${outputPath}/signal_1p25TeV/signal_1p25TeV_700mass_30MET_50pt1.root
+
+./PhenoAnalyzer ${signalPath}/VBF_m_n3_2p0_tau_decay_vbfcuts/Events/run_01/output_delphes.root ${outputPath}/signal_2p0TeV/signal_2p0TeV_700mass_30MET_50pt1.root
